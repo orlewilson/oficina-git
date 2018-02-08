721b5c07b9faf00eb91ce2bb07fd8f20c258b857
Orlewilson Maia <prof.orlewilson@gmail.com>
2018-02-07 20:42:51 -0400

    Criação do projeto base

---
+++ Exemplo Github/src/br/edu/fmf/modelo/Gato.java
@@ -0,0 +1,18 @@
+/*
+ * Oficina Git
+ * Equipe:
+ * Data: 07/02/2017
+ * Descrição: classe para representar 
+ * 			  um gato
+ */
+package br.edu.fmf.modelo;
+
+public class Gato {
+
+	// atributos
+	private String cor;
+	private String nome;
+	private int idade;
+	
+	// métodos
+}
